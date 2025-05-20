445cfb438390c2135b2d07b5b0802daff516d80b
LZHTK <gusdnjsrl2@gmail.com>
2025-05-20 11:53:44 +0900

    feature: fix 1차

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -10,21 +10,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/api/binary-contents")
+@RequestMapping("/api/binaryContents")
 @RestController
 @RequiredArgsConstructor
 public class BinaryContentController {
-    private final BinaryContentService binaryContentService;
+
+  private final BinaryContentService binaryContentService;
 
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public ResponseEntity<BinaryContent> findBinaryContentById(@PathVariable("id") UUID contentId) {
-        return ResponseEntity.ok(binaryContentService.findById(contentId));
-    }
+  @RequestMapping(value = "{id}", method = RequestMethod.GET)
+  public ResponseEntity<BinaryContent> findBinaryContentById(@PathVariable("id") UUID contentId) {
+    return ResponseEntity.ok(binaryContentService.findById(contentId));
+  }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<BinaryContent>> findAllBinaryContentByIds(@RequestBody List<UUID> contentIds) {
-        return ResponseEntity.ok(binaryContentService.findAllByIdIn(contentIds));
-    }
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<List<BinaryContent>> findAllBinaryContentByIds(
+      @RequestBody List<UUID> contentIds) {
+    return ResponseEntity.ok(binaryContentService.findAllByIdIn(contentIds));
+  }
 
 }
