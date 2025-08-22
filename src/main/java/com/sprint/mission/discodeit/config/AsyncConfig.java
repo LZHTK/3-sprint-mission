9a5ae3a0ef06d169be14f0eb688f4175801585b4
LZHTK <gusdnjsrl2@gmail.com>
2025-08-22 15:07:38 +0900

    feat: 비동기 실패 처리하기 구현

--- src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -6,14 +6,15 @@ import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 @RequiredArgsConstructor
 public class AsyncConfig {
 
--- src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
@@ -4,7 +4,9 @@ import com.sprint.mission.discodeit.entity.Notification;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     List<Notification> findAllByReceiverIdOrderByCreatedAtDesc(UUID receiverId);
 }
