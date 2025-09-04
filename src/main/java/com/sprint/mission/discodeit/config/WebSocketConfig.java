38329c326c46b9cecb2db4447846c01600c97983
LZHTK <gusdnjsrl2@gmail.com>
2025-09-04 11:42:38 +0900

    feat: 웹소켓 인증/인가 처리 구현

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,19 +1,31 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.entity.Role;
+import com.sprint.mission.discodeit.security.websocket.JwtAuthenticationChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub"); // 구독용
         config.setApplicationDestinationPrefixes("/pub"); // 발행용
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
@@ -22,5 +34,26 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
             .setAllowedOriginPatterns("*")
             .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(
+            jwtAuthenticationChannelInterceptor,
+            new SecurityContextChannelInterceptor(),
+            authorizationChannelInterceptor()
+        );
+    }
+
+    /**
+     * WebSocket 메시지에 대한 인가 정책 설정
+     * */
+    private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+        AuthorizationManager authorizationManager = MessageMatcherDelegatingAuthorizationManager.builder()
+            .anyMessage()
+            .hasRole(Role.USER.name())
+            .build();
+
+        return new AuthorizationChannelInterceptor(authorizationManager);
     }
 }
